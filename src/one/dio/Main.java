610204a89d3424bdd2c92bc41863b1b49e5b4ad0
Iury <iuryppedrosa@protonmail.com>
2022-05-24 13:15:58 -0300

    boas práticas de codificação JAVA

---
+++ src/one/dio/Main.java
@@ -0,0 +1,53 @@
+package one.dio;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i;
+        //int i;
+        int I;
+        //int 1a;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 11;
+        //j = 15;
+        int asrn246md;
+        //int asrn246 78md;
+        int asrn568_md = 19;
+        //int asrn568_md%87 = 10;
+
+        asrn246md = 100;
+        asrn568_md = 30;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        //int qtoProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+
+
+        System.out.println(j);
+        System.out.println(asrn246md);
+        System.out.println(asrn568_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+
+    }
+}
